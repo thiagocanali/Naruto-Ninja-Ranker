64ce32d2899da2e7933e207e0dec1fd734caba94
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2026-01-02 19:05:15 -0300

    ajustes vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: "/Naruto-Ninja-Ranker/",
+  base: '/Naruto-Ninja-Ranker/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
